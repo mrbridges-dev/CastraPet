2961f0ccf17e04e4d5474809dd6f162fd4aaffda
João Pontes <pontesjoao80@hotmail.com>
2025-05-15 20:22:02 -0300

    15/05/2025

--- app/components/tabela.py
+++ app/components/tabela.py
@@ -10,50 +10,50 @@ def exibir_tabela_animais():
         return
 
     # Criando um container para os cards
-    col1, col2, col3 = st.columns(3, gap="small")
+    col1, col2, col3, col4 = st.columns(4, gap="small")
     
     # Distribuindo os cards entre as colunas
     for i, animal in enumerate(animais):
         # Alternando entre as colunas
-        if i % 3 == 0:
+        if i % 4 == 0:
             col = col1
-        elif i % 3 == 1:
+        elif i % 4 == 1:
             col = col2
-        else:
+        elif i % 4 == 2:
             col = col3
+        else:
+            col = col4
         
         # Criando o card
         with col:
-                with st.container(height=200, border=True):
+                with st.container(height=260, border=True):
                     
                     # Criando colunas dentro do card
-                    img_col, info_col, button_col = st.columns(3)
+                    img_col, info_col= st.columns(2, gap="large")
                     
                     # Coluna da imagem
                     with img_col:
                         if animal['foto'] and os.path.exists(os.path.join('assets', 'img', animal['foto'])):
-                            st.image(os.path.join('assets', 'img', animal['foto']), 
-                                   width=300,
-                                   output_format="JPEG",
-                                   use_container_width=False)
+                            st.image(os.path.join('assets', 'img', animal['foto']),
+                                    width = 150,
+                                   output_format="JPEG")
                         else:
                             st.markdown("📷 Sem foto")
                     
                     # Coluna das informações
-                with info_col:
-                    st.markdown(
-                        f"""
-                        <div style="font-size:16px; line-height:1.5; margin-bottom:10px;">
-                            <strong>{animal['nome']}</strong><br>
-                            Espécie: {animal['especie']}<br>
-                            Idade: {animal['idade']} anos<br>
-                            Status: {animal['status']}
-                        </div>
-                        """,
-                        unsafe_allow_html=True
-                    )
-                
-                    with button_col:    # Botões de ação em linha única
+                    with info_col:
+                        st.markdown(
+                            f"""
+                            <div style="font-size:16px; line-height:1.5; margin-bottom:10px;">
+                                <strong>{animal['nome']}</strong><br>
+                                Espécie: {animal['especie']}<br>
+                                Idade: {animal['idade']} anos<br>
+                                Status: {animal['status']}
+                            </div>
+                            """,
+                            unsafe_allow_html=True
+                        )
+                    # Coluna dos botões
                         if st.button("✏️ Editar", key=f"edit_{animal['id']}"):
                             st.session_state.show_modal = True
                             st.session_state.editing_animal_id = animal['id']
